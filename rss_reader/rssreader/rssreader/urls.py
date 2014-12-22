b6c60bdb57645053dbea74e6a893e768ad8fcbd1
海下 直哉 <naoya-kaige@garage.co.jp>
2014-12-22 19:31:15 +0900

    fixed first commit

--- rss_reader/rssreader/rssreader/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^index/', include('user_manager.urls', namespace='user_manager')),
-    url(r'^user/', include('user_page.urls', namespace='user_page')),
-)
--- rss_reader/rssreader/user_page/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'naoya-kaige'
-from django.conf.urls import patterns, url
-from user_page import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.user_page, name='user_page'),
-                       url(r'^save/$',views.save_url, name='save_url'),
-                       url(r'^feed/$',views.read_feed, name='read_feed'),)
